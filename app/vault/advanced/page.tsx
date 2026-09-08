e2017370c11571d8c252ab85d08f9f109f3fd059
Occumed79 <Alex.Ayvazian@logan.edu>
2026-09-07 22:11:47 -0700

    Link advanced visual lab to spatial workspace

--- app/vault/advanced/page.tsx
+++ app/vault/advanced/page.tsx
@@ -5,6 +5,7 @@ export default function AdvancedVisualLabPage() {
     <>
       <AdvancedVisualLab />
       <div className="av-mode-links">
+        <a href="/vault/spatial">Spatial Lab</a>
         <a href="/vault/story">Storytelling Studio</a>
         <a href="/vault/advanced/cyber">Cyberwave preview</a>
       </div>
