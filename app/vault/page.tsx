36ee12874b3dc0571e26e6ff0261a4b72acbb6a4
Occumed79 <Alex.Ayvazian@logan.edu>
2026-09-07 23:14:01 -0700

    Replace vault hero with workspace dashboard

--- app/vault/page.tsx
+++ app/vault/page.tsx
@@ -1,13 +1,5 @@
-import DataStudioPro from '@/components/data-studio/DataStudioPro';
+import StudioDashboard from '@/components/data-studio/StudioDashboard';
 
 export default function VaultPage() {
-  return (
-    <>
-      <DataStudioPro />
-      <div className="av-mode-links">
-        <a href="/vault/grid">Grid & Pivot Lab</a>
-        <a href="/vault/advanced">Advanced Visual Lab</a>
-      </div>
-    </>
-  );
+  return <StudioDashboard />;
 }
